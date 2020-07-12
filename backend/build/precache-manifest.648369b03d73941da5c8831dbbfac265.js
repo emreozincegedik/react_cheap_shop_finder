3568f7c4ae5548cb5136a94c5c20dac3dd202970
Emre Özincegedik <emreozincegedik@gmail.com>
2020-07-12 21:32:53 +0300

    backend has react's production build

---
+++ backend/build/precache-manifest.648369b03d73941da5c8831dbbfac265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c67c16fd72e10d61379583246ee9ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3acde10fb8957c34d4e3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3acde10fb8957c34d4e3",
+    "url": "/static/js/2.d78bbb02.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d78bbb02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2680248158dcf4448b",
+    "url": "/static/js/main.a1334555.chunk.js"
+  },
+  {
+    "revision": "5a937b7930d416e14168",
+    "url": "/static/js/runtime-main.14d8df86.js"
+  }
+]);
